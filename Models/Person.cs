bbdb69b84a6f85b79c51d1dcf42e19c2e5329b17
Alex Vestelind <alex_vestelind@hotmail.com>
2022-09-26 13:20:09 +0200

    Added person class with a constructor and get-set properties. Added xUnit test

---
+++ Models/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace to_do_itv2.Models
+{
+    public class Person
+    {
+        private int idCounter = 0;
+        private readonly int id;
+        private string firstName;
+        private string lastName;
+        
+        public int PersonsCreated { get { return idCounter; } }
+        public int Id { get { return id; } }
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 2)
+                {
+                    throw new ArgumentException("First name must contain at least 2 letters");
+                }
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 2)
+                {
+                    throw new ArgumentException("First name must contain at least 2 letters");
+                }
+                lastName = value;
+            }
+        }
+        public Person(int id, string LastName, string FirstName)
+        {
+            this.id = id;
+            firstName = FirstName;
+            lastName = LastName;
+            idCounter++;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace to_do_itv2
+﻿using to_do_itv2.Models;
+
+namespace to_do_itv2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Person person1 = new Person(0, "Vestelind", "Alex");
+
+            Console.WriteLine(person1.Id + " " + person1.LastName + " " + person1.FirstName);
+            Console.WriteLine(person1.PersonsCreated);
         }
     }
 }
